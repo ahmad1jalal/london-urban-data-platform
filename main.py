612db4ef02e17de81e76a90747efca4b0094f04c
Ahmad Jalal <imahmadjalal@gmail.com>
2026-04-03 23:10:49 +0100

    Loaded processed air quality data into PostgreSQL

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from src.extract.air_quality_extract import extract_air_quality_data
 from data.raw.save_raw_data import save_raw
 from src.transform.air_quality_transform import transform_air_quality_data,save_processed_data
+from src.load.air_quality import load_data
 
 
 
 def run_pipeline():
     # df = extract_air_quality_data()
     # save_raw(df)
-    dt_transformed = transform_air_quality_data("data/raw/air_quality_2026-03-30.csv")
-    save_processed_data(dt_transformed)
+    # dt_transformed = transform_air_quality_data("data/raw/air_quality_2026-03-30.csv")
+    # save_processed_data(dt_transformed)
+    load_data("./data/processed/air_quality_2026-04-01.csv")
 
 
 if __name__  == "__main__":
---
+++ src/load/air_quality.py
@@ -0,0 +1,47 @@
+import psycopg2
+from psycopg2.extras import execute_values
+import pandas as pd
+import logging
+# setup the logging
+logging.basicConfig(filename="logs/pipeline.log",
+                    level = logging.INFO,
+                    format = "%(asctime)s - %(levelname)s -%(messsage)s")
+
+def load_data(df):
+    try : 
+        logging.info("Data loading into PostgeSQL")
+        df = pd.read_csv(df)
+        # let's connect the postgres
+        conn = psycopg2.connect(
+            dbname = "london_data",
+            user = "postgres",
+            password = "polo00",
+            host = "localhost",
+            port = "5432"
+        )
+        cur = conn.cursor()
+
+        # let's make the data point
+        data = ([row["site"],
+                row["species"],
+                row["readingdatetime"],
+                row["value"],
+                row["units"],
+                row["provisional_or_ratified"],
+                row["month"],]
+                for _,row in df.iterrows())
+
+        # let's make a query
+        query = """
+        INSERT INTO air_quality(site,species,readingdatetime,value,units,status,month) VALUES %s
+        """
+
+        # let's execute the query
+        execute_values(cur,query,data)
+        conn.commit()
+        cur.close()
+        conn.close()
+        logging.info("Data loaded successfully")
+    except Exception as e:
+        logging.error(f"Error loading data {e}")
+        raise
