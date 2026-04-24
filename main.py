5f05aac19fd7cf97e78a88534963c5b29941bd46
Ahmad Jalal <imahmadjalal@gmail.com>
2026-04-24 07:16:25 +0100

    cleaned and transform traffic dataset with feature engineering and column reduction

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from data.raw.save_raw_data import save_raw
 from src.transform.air_quality_transform import transform_air_quality_data,save_processed_data
 from src.load.air_quality import load_data
 from src.extract.traffic_raw_extract import extract_traffic_raw,save_raw_traffic_data
+from src.transform.traffic_transform import transform_data_traffic,save_transform
+
 
 
 
@@ -13,8 +15,10 @@ def run_pipeline():
     # dt_transformed = transform_air_quality_data("data/raw/air_quality_2026-03-30.csv")
     # save_processed_data(dt_transformed)
     # load_data("./data/processed/air_quality_2026-04-01.csv")
-    df = extract_traffic_raw()
-    save_raw_traffic_data(df)
+    # df = extract_traffic_raw()
+    # save_raw_traffic_data(df)
+    dt_traffic_transform = transform_data_traffic("./data/raw/traffic_2026-04-05.csv")
+    save_transform(dt_traffic_transform)
 
 
 if __name__  == "__main__":
--- src/transform/traffic_transform.py
+++ src/transform/traffic_transform.py
@@ -5,16 +5,60 @@ from datetime import datetime
 
 logging.basicConfig(
     filename = "logs/pipeline.log",
-    level = "logging.INFO",
+    level = logging.INFO,
     format = "%(asctime)s - %(levelname)s -%(message)s"
 )
 
 # transforming the data...
-def tranform_traffic(df):
-    logging.info("Transforming Traffic Data")
+def transform_data_traffic(df):
+    try :
+        logging.info("Transforming Traffic Data")
 
-    # loading data-set
-    df = pd.read_csv(df)
+        # loading data-set
+        df = pd.read_csv(df)
 
-    # standardize the column names
-    df.columns = []
+         # standardize the column names
+        df.columns = [col.strip().lower() for col in df.columns]
+
+         # dropping the duplicates
+        df = df.drop_duplicates()
+
+        # selecting the useful columns
+        cols_to_keep = [
+        "count_date",
+        "hour",
+        "road_name",
+        "region_name",
+        "local_authority_name",
+        "latitude",
+        "longitude",
+        "cars_and_taxis",
+        "buses_and_coaches",
+        "all_hgvs",
+        "all_motor_vehicles"
+    ]
+
+        df = df[cols_to_keep]
+
+        # converting the datatime
+        df["count_date"] = pd.to_datetime(df["count_date"],errors = "coerce")
+
+        # create unified datetime
+        df["datetime"] = df["count_date"] + pd.to_timedelta(df["hour"],unit = "h")
+
+        logging.info("Traffic transform successfully")
+
+        return df
+    except Exception as e:
+        logging.error(f"Error during transformation : {e}")
+        raise
+
+def save_transform(df):
+    try :
+        file_name = f"data/processed/traffic_{datetime.now().date()}.csv"
+        df.to_csv(file_name,index = False)
+        logging.info("Prcessed data saved successfully")
+    except Exception as e:
+        logging.error(f"Error occured saving processed data : {e}")
+        raise
+
