619ff926932ffe8396bd50f7c1ac031d9a97c29f
Ahmad Jalal <imahmadjalal@gmail.com>
2026-05-01 23:18:49 +0100

    traffic analysis SQL queries with window functions

--- sql/traffic_analysis.sql
+++ sql/traffic_analysis.sql
@@ -33,3 +33,30 @@ AVG(total_vehicles) As avg_traffic
 FROM fact_traffic
 GROUP BY period;
 
+--Traffic trend over time
+SELECT datetime,
+total_vehicles,
+COALESCE(LAG(total_vehicles) OVER(ORDER BY datetime),0) AS prev_traffic,
+COALESCE(total_vehicles  - LAG(total_vehicles) OVER(ORDER BY datetime),0) AS traffic_change
+FROM fact_traffic;
+
+--rolling average
+SELECT datetime,
+AVG(total_vehicles) OVER(ORDER BY datetime
+ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) AS rolling_avg
+FROM fact_traffic;
+
+-- top 10 % traffic roads
+SELECT * FROM(
+SELECT *,
+NTILE(10) OVER(ORDER BY total_vehicles DESC) AS bucket
+FROM fact_traffic
+)t
+WHERE bucket = 1;
+
+
+
+
+
+
+
